063ecd3a27fead35f4978ccd47d8943d2ba03f02
yisimple <1009341224@qq.com>
2013-07-31 16:11:13 -0700

    Update HelloWorld.java

--- JavaTest/src/com/test/java/HelloWorld.java
+++ JavaTest/src/com/test/java/HelloWorld.java
@@ -2,7 +2,6 @@ package com.test.java;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println(12);
+		System.out.println("bbb");
 	}
 }

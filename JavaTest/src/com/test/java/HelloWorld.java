97cdf39a01d386443fd45c90b90fe53dc67f9035
yisimple <1009341224@qq.com>
2013-07-31 16:32:28 -0700

    1

--- JavaTest/src/com/test/java/HelloWorld.java
+++ JavaTest/src/com/test/java/HelloWorld.java
@@ -2,7 +2,11 @@ package com.test.java;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("我的天天sdf323sdfsd");
-		System.out.println("收到货");
+		System.out.println("我的");
+		System.out.println("网站上的");
+		System.out.println("网站上的");
+		System.out.println("我的");
+		System.out.println("我的");
+		System.out.println("我的");
 	}
 }

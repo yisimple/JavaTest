e3df1de82b0ea5b515cb5cb3502d5a543adb559b
yisimple <1009341224@qq.com>
2013-07-31 16:37:48 -0700

    Update HelloWorld.java

--- JavaTest/src/com/test/java/HelloWorld.java
+++ JavaTest/src/com/test/java/HelloWorld.java
@@ -1,18 +1,14 @@
 package com.test.java;
 
 public class HelloWorld {
-	public static void main(String[] args) {
-	
-		System.out.println("网站上的");
-		System.out.println("网站上的");
-		System.out.println("网站上的");
-		System.out.println("网站上的");
-		System.out.println("网站上的");
-		System.out.println("网站上的");
-	}
+
 }
 
 class A{
 	
+	
+}
+
+class B{
 	
 }

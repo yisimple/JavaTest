6a201cb7bed1346ea8d00137ab72bdb68b4c21b7
yisimple <1009341224@qq.com>
2013-07-31 17:11:59 -0700

    Update HelloWorld.java

--- JavaTest/src/com/test/java/HelloWorld.java
+++ JavaTest/src/com/test/java/HelloWorld.java
@@ -2,9 +2,8 @@ package com.test.java;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("我1的");
-		System.out.println(122);
-		System.out.println(3);
+		System.out.println("2");
+
 	}
 
 }

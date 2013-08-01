53a25d64afb595197af1eee2bd4c429edbe9be8a
yisimple <1009341224@qq.com>
2013-07-31 17:02:38 -0700

    2

--- JavaTest/src/com/test/java/HelloWorld.java
+++ JavaTest/src/com/test/java/HelloWorld.java
@@ -3,7 +3,7 @@ package com.test.java;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("我的");
-		System.out.println(1);
+		System.out.println(122);
 		
 	}
 

3387223f6bc2cbeff8f33ae36207059e0f30ba05
yisimple <1009341224@qq.com>
2013-07-31 17:00:07 -0700

    Update HelloWorld.java

--- JavaTest/src/com/test/java/HelloWorld.java
+++ JavaTest/src/com/test/java/HelloWorld.java
@@ -3,25 +3,6 @@ package com.test.java;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("我的");
-		System.out.println("网站上的");
-		System.out.println("网站上的");
-		System.out.println("我的错了吗");
-		System.out.println("我的");
-		System.out.println("我的");
-		System.out.println("我的");
-		System.out.println("我的");
-		System.out.println("我的");
-		System.out.println("我的");
-		System.out.println("我的");
-		System.out.println("我的");
-		
-	
-		System.out.println("网站上的");
-		System.out.println("网站上的");
-		System.out.println("网站上的");
-		System.out.println("网站上的");
-		System.out.println("网站上的");
-		System.out.println("网站上的222");
 		
 	}
 

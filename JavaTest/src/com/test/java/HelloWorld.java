032931878b2470b5fb8c9b5ff601410083647d3c
zhangyijin <1009341224@qq.com>
2013-09-16 15:05:08 +0800

    测试提交

--- JavaTest/src/com/test/java/HelloWorld.java
+++ JavaTest/src/com/test/java/HelloWorld.java
@@ -6,8 +6,7 @@ public class HelloWorld {
 		System.out.println(true||false);
 		System.out.println(444);
 		System.out.println(38);
-		System.out.println("eclipse增加一行");
-		System.out.println("网络上增加一行");
+		System.out.println("回国测试");
 	}
 
 }
